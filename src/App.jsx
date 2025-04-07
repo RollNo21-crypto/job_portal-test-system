dbe65e2282d30ba6bf80d7817c27e4e10b83f646
RollNo21-crypto <gokarnkark07@gmail.com>
2025-04-07 12:29:39 +0530

    routes setup done

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,58 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLoyout from '@/layout/app-layout'
+import Landing from '@/pages/landing'
+import Onboarding from './pages/onboarding'
+import JobPage from './pages/job'
+import JobListing from './pages/job-listing'
+import PostJob from './pages/post-job'
+import SavedJobs from './pages/saved-jobs'
+import MyJobs from './pages/my-job'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLoyout />,
+    children: [
+      {
+        path: '/',
+        element: <Landing />,
+      },
+      {
+        path: '/onboarding',
+        element: <Onboarding/>,
+      },
+      {
+        path: '/job/:id',//as we dont know the id of the job we are going to use :id
+        element: <JobPage />,
+      },
+      {
+        path: '/jobs',
+        element: <JobListing />,
+      },
+      {
+        path: '/post-job',
+        element: <PostJob />,
+      },
+      {
+        path: '/saved-jobs',
+        element: <SavedJobs />,
+      },
+      {
+        path: '/my-jobs',
+        element: <MyJobs />,
+      },
+    ],
+  },
+])
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 class="text-3xl font-bold underline">
-         Hello world!
-      </h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
